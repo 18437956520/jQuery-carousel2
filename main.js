775dca14a8fb0ac71ceec94ae7b82f68117e803d
yyyh <444510796@qq.com>
2019-04-03 12:29:43 +0800

    end

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
-$('.images>img:nth-child(1)').addClass('current')
-$('.images>img:nth-child(2)').addClass('enter')
-$('.images>img:nth-child(3)').addClass('enter')
-let n = 1
+let n
+start()
 setInterval(() => {
-    $(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-        .one('transitionend', (e)=>{
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    makeLeave(getImage(n))
+        .one('transitionend', (e) => { makeEnter($(e.currentTarget)) })
+    makeCurrent(getImage(n+1))
     n += 1
 }, 2000);
 
-function x(n){
-    if(n>3){
-        n = n%3
-        if(n === 0){
-            n = 3
+/// 封装函数
+function x(n) {
+    if (n > 6) {
+        n = n % 6
+        if (n === 0) {
+            n = 6
         }
-    }// n = 1 2 3 
+    } // n = 1 2 3 
     return n
 }
+
+function getImage(n){
+    return $(`.images>img:nth-child(${x(n)})`)
+}
+
+function start() {
+    n = 1
+    $(`.images>img:nth-child(${n})`).addClass('current') .siblings().addClass('enter') 
+}
+
+function makeCurrent($node){
+    $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node){
+    $node.removeClass('enter current').addClass('leave')
+    return $node
+}
+
+function makeEnter($node){
+    $node.removeClass('current leave').addClass('enter')
+}
+
