f648f441b886ee84ba209e7ec3f04fbdb1e9de86
yyyh <444510796@qq.com>
2019-04-03 10:33:13 +0800

    依次类推简单轮播

--- main.js
+++ main.js
@@ -6,3 +6,35 @@ setTimeout(function () {
         transform: 'translateX(-100%)'
     })
 }, 2000)
+setTimeout(function () {
+    $('.images>img:nth-child(2)').css({
+        transform: 'translateX(-200%)'
+    })
+    $('.images>img:nth-child(3)').css({
+        transform: 'translateX(-100%)'
+    })
+}, 4000)
+setTimeout(function () {
+    $('.images>img:nth-child(3)').css({
+        transform: 'translateX(-200%)'
+    })
+    $('.images>img:nth-child(4)').css({
+        transform: 'translateX(-100%)'
+    })
+}, 6000)
+setTimeout(function () {
+    $('.images>img:nth-child(4)').css({
+        transform: 'translateX(-200%)'
+    })
+    $('.images>img:nth-child(5)').css({
+        transform: 'translateX(-100%)'
+    })
+}, 8000)
+setTimeout(function () {
+    $('.images>img:nth-child(5)').css({
+        transform: 'translateX(-200%)'
+    })
+    $('.images>img:nth-child(6)').css({
+        transform: 'translateX(-100%)'
+    })
+}, 10000)
