03950d2aea41efe25f6f2bf37e5f6e1a806029c3
yyyh <444510796@qq.com>
2019-04-03 10:41:21 +0800

    监听1

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ setTimeout(function () {
     $('.images>img:nth-child(2)').css({
         transform: 'translateX(-100%)'
     })
+    $('.images>img:nth-child(1)').on('transitionend', function(e){
+        $(e.currentTarget).addClass('right').css({ transform: 'none' })
+    }) //监听1结束后回到右边
 }, 2000)
+
+
 setTimeout(function () {
     $('.images>img:nth-child(2)').css({
         transform: 'translateX(-200%)'
@@ -14,27 +19,67 @@ setTimeout(function () {
         transform: 'translateX(-100%)'
     })
 }, 4000)
-setTimeout(function () {
-    $('.images>img:nth-child(3)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(4)').css({
-        transform: 'translateX(-100%)'
-    })
-}, 6000)
-setTimeout(function () {
-    $('.images>img:nth-child(4)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(5)').css({
-        transform: 'translateX(-100%)'
-    })
-}, 8000)
-setTimeout(function () {
-    $('.images>img:nth-child(5)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(6)').css({
-        transform: 'translateX(-100%)'
-    })
-}, 10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// setTimeout(function () {
+//     $('.images>img:nth-child(3)').css({
+//         transform: 'translateX(-200%)'
+//     })
+//     $('.images>img:nth-child(4)').css({
+//         transform: 'translateX(-100%)'
+//     })
+// }, 6000)
+// setTimeout(function () {
+//     $('.images>img:nth-child(4)').css({
+//         transform: 'translateX(-200%)'
+//     })
+//     $('.images>img:nth-child(5)').css({
+//         transform: 'translateX(-100%)'
+//     })
+// }, 8000)
+// setTimeout(function () {
+//     $('.images>img:nth-child(5)').css({
+//         transform: 'translateX(-200%)'
+//     })
+//     $('.images>img:nth-child(6)').css({
+//         transform: 'translateX(-100%)'
+//     })
+// }, 10000)
