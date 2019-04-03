9d8de18675726ac58d8ff33e0e1b847c65846db4
yyyh <444510796@qq.com>
2019-04-03 10:52:09 +0800

    雏形思维

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ setTimeout(function () {
     $('.images>img:nth-child(2)').css({
         transform: 'translateX(-100%)'
     })
-    $('.images>img:nth-child(1)').on('transitionend', function(e){
+    $('.images>img:nth-child(1)').one('transitionend', function(e){
         $(e.currentTarget).addClass('right').css({ transform: 'none' })
-    }) //监听1结束后回到右边
+    }) 
 }, 2000)
 
 
@@ -18,7 +18,23 @@ setTimeout(function () {
     $('.images>img:nth-child(3)').css({
         transform: 'translateX(-100%)'
     })
+    $('.images>img:nth-child(2)').one('transitionend', function(e){
+        $(e.currentTarget).addClass('right').css({ transform: 'none' })
+    }) 
 }, 4000)
+
+
+setTimeout(function () {
+    $('.images>img:nth-child(3)').css({
+        transform: 'translateX(-200%)'
+    })
+    $('.images>img:nth-child(1)').css({
+        transform: 'translateX(-100%)'
+    })
+    $('.images>img:nth-child(3)').one('transitionend', function(e){
+        $(e.currentTarget).addClass('right').css({ transform: 'none' })
+    }) 
+}, 6000)
 
 
 
